8ef413b7a5e04e5c50789452d3d079879e9d8058
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-07-02 10:22:02 +0200

    html; et input terminé sur les limitations de caractères

---
+++ script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // $("#btn_submit").click(inputLength);
+    // var psd = $("#user_pseudo").val();
+    // function inputLength(){
+    //     if(psd.length < 5){
+    //         alert('Veuillez');
+    //         return false;
+    //     }
+    // }
+    // console.log(psd);
+    
+});
