f4169bcecf1be8f9f4f747a6ed3188838fd28e0a
Mordnor <dorian.pitiot@gmail.com>
2018-07-02 11:15:23 +0200

    ajout function verif mdp et form remplis

--- script/script.js
+++ script/script.js
@@ -1,12 +1,26 @@
-$(document).ready(function(){
-    // $("#btn_submit").click(inputLength);
-    // var psd = $("#user_pseudo").val();
-    // function inputLength(){
-    //     if(psd.length < 5){
-    //         alert('Veuillez');
-    //         return false;
-    //     }
-    // }
-    // console.log(psd);
-    
+$(document).ready(function() {
+
+    // Verification des mots de passe
+    $("#btn_submit").click(verifSubmit)
+
+    function verifSubmit() {
+
+        $("input").each(function(i){
+            if ($("input").val() == false) {
+              $(this).css("border", "1px solid red")
+            }
+        });
+
+        if ($("#input_password").val() != $("#confirm_password").val()) {
+            alert("Veuillez entrer des mots de passe identique");
+        }
+        return false;
+    }
+    // verification du pseudo
+    $("#user_pseudo").focus(verifPseudo)
+
+    function verifPseudo() {
+
+    }
+
 });
