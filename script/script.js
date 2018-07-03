f623e7e4fa4d651478808ed002d08c02a8d68681
Toussaint Dimitry <dimitrytoussaint2@gmail.com>
2018-07-03 11:35:10 +0200

    verification mail

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     // Verification des mots de passe
-    $("#btn_submit").click(verifSubmit)
+    $("#email").click(verifSubmit)
     var allInputs = $("input");
 
     function verifSubmit() {
@@ -34,4 +34,26 @@ $(document).ready(function() {
         }
 
     }
+
+    function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  var $result = $("#result");
+  var email = $("#email").val();
+  $result.text("");
+
+  if (validateEmail(email)) {
+    $result.text(email + " is valid :)");
+    $result.css("color", "green");
+  } else {
+    $result.text(email + " is not valid :(");
+    $result.css("color", "red");
+  }
+  return false;
+}
+
+$("#validate").bind("click", validate);
 });
